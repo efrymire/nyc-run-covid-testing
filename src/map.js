79f319e52fe920ff46565ff38fcc24d2a6f41087
efrymire <ellie.frymire@gmail.com>
2021-12-22 14:15:20 -0800

    try with NODE_ENV_

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 class Mapbox {
 
   constructor(centers) {
-    mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+    mapboxgl.accessToken = process.env.NODE_ENV_MAPBOX_ACCESS_TOKEN;
     const map = new mapboxgl.Map({
       container: 'map', // container ID
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
